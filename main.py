155a2126c4c5adb7cc3e9a63cb9f3fd571626827
the44 <taytro771@gmail.com>
2026-03-07 02:42:06 +0300

    Add root endpoint and run server with uvicorn

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI, Header, HTTPException, Request
 import subprocess
+import uvicorn
 
 app = FastAPI()
 API_KEY = "NEON-EXOS-ULTIMATE-2026"
+
+@app.get("/")
+async def root():
+    return {"status": "Neon Server is Live"}
 
 @app.post("/server/terminal")
 async def execute(request: Request, x_api_key: str = Header(None)):
@@ -12,3 +17,6 @@ async def execute(request: Request, x_api_key: str = Header(None)):
     cmd = data.get("cmd")
     result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
     return {"stdout": result.stdout, "stderr": result.stderr}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
